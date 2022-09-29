5cf8aa5eaf17d6e436ae9d0d40f19b9b1022d60d
salveegu <salveegu@gmail.com>
2022-09-29 13:39:24 -0300

    002

--- Table.js
+++ Table.js
@@ -1,6 +1,6 @@
 class Table {
   constructor(dadosProcesados) {
-    this.header = arr[0]; //peguei o cabecalho- que sao as categorias da tabela
+    this.header = dadosProcesados[0]; //peguei o cabecalho- que sao as categorias da tabela
     dadosProcesados.shift(); //removi o cabecalho pois ja usei ele - que o corpo/os ddos
     this.rows = dadosProcesados;
   }
--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const Reader = require("./Reader");
 const Processor = require("./Processor");
-const Table =  require ("./Table");
+const Table = require("./Table");
 
 async function main() {
- 
- var dados = await leitor.Read("./produtos.csv");
+  
+  var leitor = new Reader();
+
+  var dados = await leitor.Read("./produtos.csv");
 
   var dadosProcessados = Processor.Process(dados);
+  
   var usuarios = new Table(dadosProcessados);
 
-  console.log(usuarios.header);  
-
+  console.log(usuarios.header);
+  console.log(usuarios.rows);
 }
 
 main();
- 
